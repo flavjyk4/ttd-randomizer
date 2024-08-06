550fa69acd5ab1468054e533ccc9e52cdde19f20
ihatespawn <168680471+ihatespawn@users.noreply.github.com>
2024-08-06 13:02:43 +0200

    v1

---
+++ config.py
@@ -0,0 +1,37 @@
+units = open('jednostki.txt').readlines()
+
+maps = [
+    'toilet city',
+    'toilet hq',
+    'toilet lab',
+    'desert',
+    'palm paradise',
+    'toilet fortress'
+]
+
+modes = [
+    'nightmare',
+    'hard',
+    'endless'
+]
+
+impediments = [
+    'wywal 1 postac',
+    'wywal 2 postacie',
+    'wywal najlepsza postac',
+    'mozesz postawic max 10 postaci',
+    'tylko auto skip',
+    'nie mozesz stawiac farm',
+    'tylko 10 ulepszen',
+    'wszystko na 1 poziomie',
+    'tylko 25 ulepszen',
+    'mozesz stawiac dopiero od 2k kasy',
+    'mozesz stawiac dopiero od 1k kasy',
+    'auto skip do fali 10',
+    'auto skip do fali 30',
+    'przelosuj 1 postac',
+    'przelosuj 2 postacie',
+    'nie uzywasz umiejetnosci (postacie)',
+    'nie uzywasz umiejetnosci (endless)',
+    'nie uzywasz zadnych skilli'
+]
---
+++ main.py
@@ -0,0 +1,34 @@
+from keyboard import on_press, wait
+from secrets import choice
+from config import *
+from sys import exit
+newline = '\n'
+
+def rollall():
+    print(f'Mapa: {choice(maps)}')
+    print(f'Tryb: {choice(modes)}')
+
+    for n in range(6):
+        print(f'Postać {n}: {choice(units).removesuffix(newline)}')
+
+    for n in range(2):
+        print(f'Utrudnienie {n}: {choice(impediments)}')
+
+def onkey(event):
+    try:
+        key = int(event.name)
+        if key > 0 and key < 7:
+            print(f'Postać {key}: {choice(units).removesuffix(newline)}')
+        elif key > 6 and key < 9:
+            print(f'Utrudnienie {key}: {choice(impediments)}')
+        else:
+            rollall()
+    except ValueError:
+        pass
+
+rollall()
+on_press(onkey)
+try:
+    wait()
+except KeyboardInterrupt:
+    print("Zatrzymano.")
